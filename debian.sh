c3db892daaf09ab14b59e637b91b7c9f4cf4c666
Hiromasa Ihara <miettal@debian>
2014-10-15 12:29:53 +0900

    fix

--- debian.sh
+++ debian.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
+apt-get install --assume-yes git
 apt-get install --assume-yes tmux
 apt-get install --assume-yes zsh
 apt-get install --assume-yes vim
-apt-get install --assume-yes curl
 apt-get install --assume-yes ctags
+
+apt-get install --assume-yes wget
+apt-get install --assume-yes curl
+
+# for python
+apt-get install --assume-yes libssl-dev
+apt-get install --assume-yes zlib1g-dev
+apt-get install --assume-yes libbz2-dev
+apt-get install --assume-yes libreadline-dev
+apt-get install --assume-yes libsqlite3-dev
+
+# for ruby
+apt-get install --assume-yes libssl-dev
+apt-get install --assume-yes libreadline-dev
