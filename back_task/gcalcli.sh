d412dcb6ecc863e589b8ab694e308b596abb8792
Hiromasa Ihara <iharahiromasa@gmail.com>
2017-09-04 14:04:53 +0900

    quick commit

--- back_task/gcalcli.sh
+++ back_task/gcalcli.sh
@@ -1,5 +1,6 @@
 export PATH=$HOME/.pyenv/bin:$HOME/.pyenv/shims:$PATH
 eval "$(pyenv init -)"
+export LC_ALL="ja_JP.UTF-8"
 
 gcalcli agenda --military > /tmp/com.miettal.gcalcli.$$
 cp -f /tmp/com.miettal.gcalcli.$$ /tmp/com.miettal.gcalcli
