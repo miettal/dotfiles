023d0f475ac6fa2e4ff4c26feabd45c69ccc8c36
Hiromasa Ihara <iharahiromasa@gmail.com>
2018-07-23 10:05:48 +0900

    quick commit

--- mac/install.sh
+++ mac/install.sh
@@ -3,12 +3,15 @@
 brew install git
 brew install mercurial
 brew install tmux
-brew install vim
-brew install neovim
 brew install coreutils
 brew install reattach-to-user-namespace
 brew tap brona/iproute2mac
 brew install iproute2mac
+
+# for c
 brew install autoconf
 brew install automake
 
+# for vim
+brew install neovim
+brew install vim
