059bfebc4a9a6c7d321522cbc591363cb6064998
Hiromasa Ihara <iharahiromasa@gmail.com>
2018-10-07 22:29:11 +0900

    quick commit

--- config.fish
+++ config.fish
@@ -1,12 +1,12 @@
 fish_vi_key_bindings
 
 # pyenv
-set -x PATH $HOME/.pyenv/bin $PATH
-set -x PATH $HOME/.pyenv/shims $PATH
+set -U fish_user_paths $HOME/.pyenv/bin $fish_user_paths
+set -U fish_user_paths $HOME/.pyenv/shims $fish_user_paths
 . (pyenv init - --no-rehash | psub)
-#rbenv
-set -x PATH $HOME/.rbenv/bin $PATH
-set -x PATH $HOME/.rbenv/shims $PATH
+# rbenv
+set -U fish_user_paths $HOME/.rbenv/bin $fish_user_paths
+set -U fish_user_paths $HOME/.rbenv/shims $fish_user_paths
 . (rbenv init - --no-rehash | psub)
 
 alias commit='git add .; git commit . -m "quick commit"; git push'
