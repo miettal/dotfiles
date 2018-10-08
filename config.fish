32cff328cf1eef3d59a8c1cdbd123ff6a2449744
Hiromasa Ihara <iharahiromasa@gmail.com>
2018-10-08 17:39:11 +0900

    quick commit

--- config.fish
+++ config.fish
@@ -1,13 +1,13 @@
 fish_vi_key_bindings
 
-# pyenv
-set -U fish_user_paths $HOME/.pyenv/bin $fish_user_paths
-set -U fish_user_paths $HOME/.pyenv/shims $fish_user_paths
-. (pyenv init - --no-rehash | psub)
-# rbenv
-set -U fish_user_paths $HOME/.rbenv/bin $fish_user_paths
-set -U fish_user_paths $HOME/.rbenv/shims $fish_user_paths
-. (rbenv init - --no-rehash | psub)
+# # pyenv
+# set -U fish_user_paths $HOME/.pyenv/bin $fish_user_paths
+# set -U fish_user_paths $HOME/.pyenv/shims $fish_user_paths
+# . (pyenv init - --no-rehash | psub)
+# # rbenv
+# set -U fish_user_paths $HOME/.rbenv/bin $fish_user_paths
+# set -U fish_user_paths $HOME/.rbenv/shims $fish_user_paths
+# . (rbenv init - --no-rehash | psub)
 
 alias commit='git add .; git commit . -m "quick commit"; git push'
 
--- mac/install.sh
+++ mac/install.sh
@@ -2,20 +2,18 @@
 
 brew install git
 brew install mercurial
-brew install tmux
-brew install coreutils
-brew install reattach-to-user-namespace
 brew tap brona/iproute2mac
 brew install iproute2mac
+
+# for shell
+brew install fish
+brew install tmux
+brew install coreutils
 
 # for c
 brew install autoconf
 brew install automake
-<<<<<<< HEAD
-brew install fish
-=======
 
 # for vim
 brew install neovim
 brew install vim
->>>>>>> 1bdb335b91d85de3cf59386d7337185da4a845c5
