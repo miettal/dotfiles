8edc58c09b7f9c05a3b561f5101b2f7d9753296c
Hiromasa Ihara <iharahiromasa@gmail.com>
2018-10-11 16:16:47 +0900

    quick commit

--- config.fish
+++ config.fish
@@ -6,7 +6,10 @@ set -U fish_user_paths $HOME/.pyenv/bin $fish_user_paths
 set -U fish_user_paths $HOME/.rbenv/bin $fish_user_paths
 set -U fish_user_paths $HOME/.local/bin $fish_user_paths
 
+set -x EDITOR vim
+
 alias commit='git add .; git commit . -m "quick commit"; git push'
+alias vi='vim'
 
 if status is-interactive
 and not set -q TMUX
