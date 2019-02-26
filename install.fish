aab5476cce131900e9a0bdbb8dac3041155aaf96
Hiromasa Ihara <iharahiromasa@gmail.com>
2019-02-27 01:04:43 +0900

    quick commit

--- install.fish
+++ install.fish
@@ -3,4 +3,4 @@
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 set -x PATH $HOME/.pyenv/bin $PATH
 set -x PATH $HOME/.rbenv/bin $PATH
-fisher add
+fisher add oh-my-fish/theme-bobthefish
